41f9cad3b7edfedb996bfbe386d2c27a11065cd8
bikky <bikkydahal@gmail.com>
2024-05-20 15:37:54 +0545

    Comment model created along with related basic templates

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.db.models.functions import Now
 
@@ -17,7 +18,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     # publish = models.DateTimeField(default=timezone.now)
     publish = models.DateTimeField(db_default=Now())    
     created = models.DateTimeField(auto_now_add=True)
@@ -48,3 +49,34 @@ class Post(models.Model):
         return self.title
     
     
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+    )
+        
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        ordering = ['created']
+        indexes = [
+            models.Index(fields=['created']),
+        ]
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,138 @@
 from django.shortcuts import render, get_object_or_404
-
-from .models import Post
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .models import  Post
 from django.http import Http404
+from django.views.generic import ListView
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
-def post_detail(request, id):
+from .forms import CommentForm, EmailPostForm
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
     post = get_object_or_404(
         Post,
-        id=id,
+        id=post_id,
         status=Post.Status.PUBLISHED
     )
-    
-    return render(
-    request,
-    'blog/post/detail.html',
-    {'post': post}
-    )
-
-
-def post_list(request):
-    posts = Post.published.all()
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']]
+            )
+            sent = True
+            # ... send email
+    else:
+        form = EmailPostForm()
     return render(
         request,
-        'blog/post/list.html',
-        {'posts': posts}
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        }
     )
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
+    
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    
+    return render(
+        request,
+        'blog/post/detail.html',
+        {
+            'post': post,
+            'comments': comments,
+            'form': form
+        }
+    )
+    
+    
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts}
+#     )
